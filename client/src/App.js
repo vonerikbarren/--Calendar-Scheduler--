d55e07e7a748bcf54314a11c8e940320f453a9ce
Von'Erik Barren-Tucker <barrentucker.vonerik@gmail.com>
2020-04-27 19:06:52 -0400

    Initialization of the create-react-app

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div> Hello React </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
