b559cdabfed4e5e72f1e16291a1adb23e90090a7
Von'Erik Barren-Tucker <barrentucker.vonerik@gmail.com>
2020-04-28 10:45:34 -0400

    Start refactoring the code for initialization of material-ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,12 @@
+// --> Initialization of React <--
 import React from 'react';
 import './App.css';
+
+// --> Initializing Material-UI <--
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 
 function App() {
   return (
