14296c5c1fab7ebf791917daffa31ec7d04772b5
Von'Erik Barren-Tucker <barrentucker.vonerik@gmail.com>
2020-04-28 10:52:21 -0400

    Created the Grid for the Spaces needed for the full application

--- client/src/App.js
+++ client/src/App.js
@@ -8,12 +8,63 @@ import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 
+// --> Initialization of makeStyles from Material-UI <--
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+    backgroundColor: "grey"
+  },
+  grid: {
+    backgroundColor: "black"
+  },
+  button: {
+    backgroundColor: "#282c34",
+    color: "white",
+  }
+
+
+
+}));
+
+
 function App() {
+  const mui = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <div className="jumbotron border bg-warning">Hello React </div>
-      </header>
+    <div className="App-header">
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Paper elevation={3} className={mui.paper}>
+            <Paper elevation={3} className={mui.paper}>
+              <Typography> Hello React </Typography>
+            </Paper>
+          </Paper>
+        </Grid>
+
+        <Grid item xs={12}>
+          <Paper elevation={3} className={mui.paper}>
+            <Paper elevation={3} className={mui.paper}>
+              <Typography> Hello React </Typography>
+            </Paper>
+          </Paper>
+        </Grid>
+
+        <Grid item xs={12}>
+          <Paper elevation={3} className={mui.paper}>
+            <Paper elevation={3} className={mui.paper}>
+              <Typography> Hello React </Typography>
+            </Paper>
+          </Paper>
+        </Grid>
+
+
+      </Grid>
+
     </div>
   );
 }
