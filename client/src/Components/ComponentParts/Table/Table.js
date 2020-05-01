c140408e862aba7afa319f9135331db6147ca64f
Von'Erik Barren-Tucker <barrentucker.vonerik@gmail.com>
2020-04-30 22:02:08 -0400

    imported all mui components needed for set testing

--- client/src/Components/ComponentParts/Table/Table.js
+++ client/src/Components/ComponentParts/Table/Table.js
@@ -3,6 +3,10 @@ import React from 'react';
 
 
 // --> Material-UI specific <--
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 
 // --> Components imported <--
 
